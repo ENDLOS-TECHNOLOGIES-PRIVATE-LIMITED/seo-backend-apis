199f48c55de1f3871e0ac0b27838f8a96f4a8079
“hiteshendlos” <hitesh.endlos@gmail.com>
2023-06-23 11:15:52 +0530

    new updates done

--- src/api/models/head.ts
+++ src/api/models/head.ts
@@ -5,7 +5,16 @@ const headSchema = new Schema(
   {
     website: { type: mongoose.Schema.Types.ObjectId, refPath: "websiteRef" },
     webPageUrl: { type: String, required: true,unique:true },
-    headTag: { type: String, required: true,  },
+    title: { type: String, required: true,  },
+    description: { type: String, required: true,  },
+    keywords: { type: String, required: true,  },
+    ogTitle: { type: String, required: true,  },
+    ogDescription: { type: String, required: true,  },
+    ogImageUrl: { type: String, required: true,  },
+    twitterTitle: { type: String, required: true,  },
+    twitterDescription: { type: String, required: true,  },
+    twitterImage: { type: String, required: true,  },
+
   },
   {
     timestamps: true,
--- src/api/validations/head.ts
+++ src/api/validations/head.ts
@@ -2,9 +2,17 @@ const yup = require("yup");
 
 // Validation schema using Yup
 const headSchema = yup.object().shape({
-  webPageUrl: yup.string().required().min(2),
-
-  headTag: yup.string().required("Head Tag Required"),
+  website: yup.string().required("Website is required"),
+  webPageUrl: yup.string().required("Web Page URL is required").min(2),
+  title: yup.string().required("Title is required"),
+  description: yup.string().required("Description is required"),
+  keywords: yup.string().required("Keywords are required"),
+  ogTitle: yup.string().required("Open Graph Title is required"),
+  ogDescription: yup.string().required("Open Graph Description is required"),
+  ogImageUrl: yup.string().required("Open Graph Image URL is required"),
+  twitterTitle: yup.string().required("Twitter Title is required"),
+  twitterDescription: yup.string().required("Twitter Description is required"),
+  twitterImage: yup.string().required("Twitter Image URL is required")
 });
 
 
